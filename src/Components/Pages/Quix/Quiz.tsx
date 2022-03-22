4cc438fe3eac92a1c88c3f02e3105abecfca547e
Sajib Hosen <sajib.201h@gmail.com>
2022-03-23 01:36:03 +0600

    page pointer done

--- src/Components/Pages/Quix/Quiz.tsx
+++ src/Components/Pages/Quix/Quiz.tsx
@@ -9,7 +9,7 @@ import QuestionThree from './Questions/QuestionThree';
 import QuestionTwo from './Questions/QuestionTwo';
 import QuestionFive from './Questions/QuestionFive';
 import Result from './Result/Result';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Quiz = () => {
     const cStudent = useSelector((state: RootState): studentTypes => state.currentStudents.value) // getting stored data from redux store
@@ -28,11 +28,22 @@ const Quiz = () => {
     },[cStudent])
 
     return (
-        <div>
-            {/* <p>Student Name: {cStudent.name}, Student ID: {cStudent.id}</p>
-            <p>Preferred Language: {cStudent.language}, Student Gender: {cStudent.gender}</p> */}
+        <div><br />
+        {queNum >= 5 ||  <div style={{ display:'flex', justifyContent:'space-around',}}>
+                <div style={{ width:'350px', display:'flex', justifyContent:'space-around', alignItems: 'center' }}>
+                    <div style={{ border:"1px solid black", width: '15px', height: '15px', borderRadius:'50%', backgroundColor:`${queNum === 0 ? '#4A0899' : ''}`}}></div>
+                    <div style={{ border:"1px solid black", width: '15px', height: '15px', borderRadius:'50%', backgroundColor:`${queNum === 1 ? '#4A0899' : ''}`}}></div>
+                    <div style={{ border:"1px solid black", width: '15px', height: '15px', borderRadius:'50%', backgroundColor:`${queNum === 2 ? '#4A0899' : ''}`}}></div>
+                    <div style={{ border:"1px solid black", width: '15px', height: '15px', borderRadius:'50%', backgroundColor:`${queNum === 3 ? '#4A0899' : ''}`}}></div>
+                    <div style={{ border:"1px solid black", width: '15px', height: '15px', borderRadius:'50%', backgroundColor:`${queNum === 4 ? '#4A0899' : ''}`}}></div>
+                </div>
+            </div>}
+           
             {
-                (queNum === 0 && <QuestionOne currentStudent = {cStudent} />) || (queNum === 1 && <QuestionTwo currentStudent = {cStudent} /> ) || (queNum === 2 && <QuestionThree currentStudent = {cStudent} /> ) || (queNum === 3 && <QuestionFour currentStudent = {cStudent} /> ) || (queNum === 4 && <QuestionFive currentStudent = {cStudent} /> ) || (queNum === 5 && <Result currentStudent = {cStudent} />) || (queNum >= 6 && <p>Thank You</p>)
+                (queNum === 0 && <QuestionOne currentStudent = {cStudent} />) || (queNum === 1 && <QuestionTwo currentStudent = {cStudent} /> ) || (queNum === 2 && <QuestionThree currentStudent = {cStudent} /> ) || (queNum === 3 && <QuestionFour currentStudent = {cStudent} /> ) || (queNum === 4 && <QuestionFive currentStudent = {cStudent} /> ) || (queNum === 5 && <Result currentStudent = {cStudent} />) || (queNum >= 6 && <div>
+                    <p>Thank You !</p>
+                    <Link to='/'>Get back to home</Link>
+                </div>)
             }
             { queNum >= 6 || <Button variant="outlined" onClick={handleNext}>Next &gt;&gt;</Button> }
         </div>
